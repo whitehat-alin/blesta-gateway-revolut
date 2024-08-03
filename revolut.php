c6bff9fc6e3506bae43df27c04400afe0ea4f3d1
whitehat-alin <95694497+whitehat-alin@users.noreply.github.com>
2024-08-03 17:17:02 +0300

    Update revolut.php

--- revolut.php
+++ revolut.php
@@ -2,9 +2,10 @@
 class Revolut extends NonmerchantGateway {
     private $meta;
     public function __construct() {
-        $this->loadConfig(dirname(__FILE__) . DS . 'config.json');
-        Loader::loadComponents($this, ['Input']);
-        Loader::loadHelpers($this, ['Html']);
+        $this->loadConfig(dirname(__FILE__) . DS . "config.json");
+        Loader::loadComponents($this, ["Input"]);
+        Loader::loadHelpers($this, ["Html"]);
+        Language::loadLang("revolut", null, dirname(__FILE__) . DS . "language" . DS);
     }
 
     public function setMeta(array $meta = null) {
@@ -12,7 +13,11 @@ class Revolut extends NonmerchantGateway {
     }
 
     public function getName() {
-        return "Revolut";
+        return "Revolut Pay";
+    }
+
+    public function getDescription() {
+        return "Revolut Pay is an online payment processing service that helps you accept credit cards.";
     }
 
     public function getVersion() {
@@ -20,19 +25,33 @@ class Revolut extends NonmerchantGateway {
     }
 
     public function getAuthors() {
-        return "WhiteHat";
+        return array(array("name" => "WhiteHat", "url" => "https://whitehat.ro"));
+    }
+
+    public function getLogo() {
+        return null;
     }
 
     public function getSettings(array $meta = null) {
-        return [];
+        // Load the view into this object, so helpers can be automatically add to the view
+        $this->view = new View("settings", "default");
+        $this->view->setDefaultView("components" . DS . "gateways" . DS . "nonmerchant" . DS . "revolut" . DS);
+
+        // Load the helpers required for this view
+        Loader::loadHelpers($this, ["Form", "Html"]);
+
+        $this->view->set("meta", $meta);
+
+        return $this->view->fetch();
     }
 
     public function editSettings(array $meta) {
         $rules = [
-            'merchant_key' => [
-                'empty' => [
-                    'rule' => 'isEmpty',
-                    'message' => 'Code empty'
+            "merchant_key" => [
+                "empty" => [
+                    "rule" => "isEmpty",
+                    "negate" => true,
+                    "message" => "Code empty"
                 ]
             ]
         ];
@@ -42,7 +61,7 @@ class Revolut extends NonmerchantGateway {
     }
 
     public function encryptableFields() {
-        return ['merchant_key'];
+        return ["merchant_key"];
     }
 
     public function setCurrency($currency) {
@@ -51,23 +70,30 @@ class Revolut extends NonmerchantGateway {
 
     public function buildProcess(array $contact_info, $amount, array $invoice_amounts = null, array $options = null) {
         $amount = round($amount, 2);
-        Loader::loadModels($this, ['Contacts']);
+        Loader::loadModels($this, ["Contacts"]);
 
         $params = [
-            'amount' => $amount.'00',
-            'currency' => $this->currency,
-            'description' => $options['description'] ?? 'Payment',
-            'redirect_url' => Configure::get('Blesta.gw_callback_url') . Configure::get('Blesta.company_id') . '/revolut/?client_id='. ($contact_info['client_id'] ?? null) .'&amount='. ($amount ?? null)),
-            'merchant_order_data' => [
-                'reference' => (uniqid() ?? null)
+            "amount" => $amount."00",
+            "currency" => $this->currency,
+            "description" => $options["description"] ?? "Payment",
+            "redirect_url" => Configure::get("Blesta.gw_callback_url") . Configure::get("Blesta.company_id") . "/revolut",
+            "merchant_order_data" => [
+                "reference" => (uniqid() ?? null)
             ],
         ];
 
         try {
             $curl = curl_init();
             curl_setopt_array($curl, array(
-                CURLOPT_URL=>'https://merchant.revolut.com/api/orders',CURLOPT_RETURNTRANSFER=>1,CURLOPT_HEADER=>0,CURLOPT_CUSTOMREQUEST=>'POST',CURLOPT_POSTFIELDS=>json_encode($params),
-                CURLOPT_HTTPHEADER=>array('Content-Type: application/json','Accept: application/json','Authorization: Bearer ". $this->meta['merchant_key'] ."','Revolut-Api-Version: 2023-09-01'),
+                CURLOPT_URL => "https://merchant.revolut.com/api/orders",
+                CURLOPT_RETURNTRANSFER => 1,
+                CURLOPT_HEADER => 0,
+                CURLOPT_CUSTOMREQUEST=> "POST",
+                CURLOPT_POSTFIELDS => json_encode($params),
+                CURLOPT_HTTPHEADER => array("Content-Type: application/json",
+                                            "Accept: application/json",
+                                            "Authorization: Bearer ". $this->meta["merchant_key"],
+                                            "Revolut-Api-Version: 2023-09-01"),
             ));
             $response = curl_exec($curl);
             curl_close($curl);
@@ -76,8 +102,8 @@ class Revolut extends NonmerchantGateway {
             $this->Input->setErrors([$e->getMessage()]);
         }
 
-        $this->view = $this->makeView('process', 'default', str_replace(ROOTWEBDIR, '', dirname(__FILE__) . DS));
-        $this->view->set('invoice_url', $params['redirect_url'] ?? null);
+        $this->view = $this->makeView("process", "default", str_replace(ROOTWEBDIR, "", dirname(__FILE__) . DS));
+        $this->view->set("invoice_url", $invoice->checkout_url ?? null);
 
         return $this->view->fetch();
     }
@@ -91,10 +117,10 @@ class Revolut extends NonmerchantGateway {
     }
 
     public function refund($reference_id, $transaction_id, $amount, $notes = null) {
-        $this->Input->setErrors($this->getCommonError('unsupported'));
+        $this->Input->setErrors($this->getCommonError("unsupported"));
     }
 
     public function void($reference_id, $transaction_id, $notes = null) {
-        $this->Input->setErrors($this->getCommonError('unsupported'));
+        $this->Input->setErrors($this->getCommonError("unsupported"));
     }
 }
